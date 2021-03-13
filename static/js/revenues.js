519d42409e929dfef64274c9940e8dd2b29c6e17
Claude-Hanfou <claudehanfou@gmail.comgit config --global user.name Claude-Hanfougit config --global user.email you@example.com  git config --global user.name Your>
2021-03-13 13:58:28 -0600

    modified slider

--- static/js/revenues.js
+++ static/js/revenues.js
@@ -267,7 +267,7 @@ function createSliderUI(timestamps) {
 
 function createTemporalLegend(startTimestamp) {
 
-    var temporalLegend = L.control({ position: 'bottomright' });
+    var temporalLegend = L.control({ position: 'bottomleft' });
 
     temporalLegend.onAdd = function (map) {
         var output = L.DomUtil.create('output', 'temporal-legend');
